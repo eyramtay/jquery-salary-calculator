564051ecc5a7de5de558c53ba455f4a0d537ebc1
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-04-10 12:00:53 -0500

    added array of objects

--- client.js
+++ client.js
@@ -2,6 +2,53 @@ console.log('--- Weekend Project ---');
 
 $(document).ready(readyNow);
 
+const startingEmployees = [
+    {
+        firstName: 'Jen', 
+        lastName: 'Barber',
+        iD: 4521,
+        title: 'Team Lead',
+        annualSalary: 80000,
+    },
+    {
+        firstName: 'Maurice', 
+        lastName: 'Moss',
+        iD: 8724,
+        title: 'Support Team',
+        annualSalary: 58000,
+    },
+    {
+        firstName: 'Roy', 
+        lastName: 'Smith',
+        iD: 9623,
+        title: 'Quality Assurance',
+        annualSalary: 48000,
+    }
+];
+
 function readyNow() {
-    
+    // addEmployeeButton();
+    $( '#add-employee' ).on('click', addEmployeeButton);
 }
+
+function addEmployeeButton() {
+    console.log('In addEmployee');
+
+    let newEmployee = {
+        firstName: $('#in-first-name').val(),
+        lastName: $('#in-last-name').val(),
+        iD: $('#in-ID').val(),
+        title: $('#in-title').val(),
+        annualSalary: $('#in-salary').val(),
+    }
+        $('.employees').append(`<tr>
+            <td>${newEmployee.firstName}</td>
+            <td>${newEmployee.lastName}</td>
+            <td>${newEmployee.iD}</td>
+            <td>${newEmployee.title}</td>
+            <td>${newEmployee.annualSalary}</td>
+            <td>&nbsp;</td>
+        </tr>`)
+        
+        $('#in-first-name').val();
+    }
